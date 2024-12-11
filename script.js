697f3d692ae188c90fb3d82c3d1a61760441bf52
am23aco <am23aco@herts.ac.uk>
2024-12-11 21:49:41 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openProjectDetails(project) {
+    if (project === 'project1') {
+        window.location.href = "project-details.html?project=project1";
+    } else if (project === 'project2') {
+        window.location.href = "project-details.html?project=project2";
+    }
+}
